b3daa7e750b0da9a79cc02937c606f25de73f680
neverlock <neverlock@gmail.com>
2015-04-30 15:11:59 +0700

    add goroutine example

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 *** USE
 - edit chaincmd.gcfg for your command and arg
 - in chaincmd.gcfg if have any ID program will replace ID with request from HTTP GET request http://xxx/run?ID=brabrabra
+- add example use goroutine 
---
+++ main_goroutine.go
@@ -0,0 +1,76 @@
+package main
+import (
+	"fmt"
+	"log"
+	"html"
+	"strings"
+	"os/exec"
+	"net/http"
+	"github.com/gorilla/mux"
+	"code.google.com/p/gcfg"
+	"bytes"
+	)
+
+
+type Config struct {
+	Copycat struct {
+	Cmd string
+	Arg string
+	Arg1 string
+	Bind string
+	}
+}
+
+var cfg Config
+
+func httplog(r *http.Request){
+        log.Printf("%s - %s - %s - %s - %q",
+                r.RemoteAddr,
+                r.Proto,
+                r.Method,
+                r.UserAgent(),
+                html.EscapeString(r.URL.Path),
+        )
+}
+
+
+
+func main(){
+
+	err := gcfg.ReadFileInto(&cfg, "chaincmd.gcfg")
+	if err != nil {
+	    log.Fatalf("Failed to parse gcfg data: %s", err)
+	}
+	//var outPut chan string = make(chan string)
+
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/run",runcmd).Methods("GET").Queries("ID","{ID}")
+        http.Handle("/", rtr)
+	bind := cfg.Copycat.Bind
+        fmt.Printf("listening on %s...\n", bind)
+        http.ListenAndServe(bind, nil)
+}
+
+func execmd(cmd string,arg1 string,arg2 string) {
+	exe := exec.Command(cmd,arg1,arg2)
+	var out bytes.Buffer
+	exe.Stdout = &out
+	err := exe.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("STDOUT:", out.String())
+}
+
+func runcmd(w http.ResponseWriter, r *http.Request) {
+	httplog(r)
+	params := mux.Vars(r)
+	Arg := strings.Replace(cfg.Copycat.Arg,"ID",params["ID"],-1)
+	log.Println("Arg := ",Arg)
+
+	go execmd(cfg.Copycat.Cmd,Arg,cfg.Copycat.Arg1)
+
+	ret := fmt.Sprintf("{\"Cmd\":\"%s\",\"Arg\":\"%s\"}",cfg.Copycat.Cmd,Arg)
+	w.Header().Set("Content-Type", "application/json;charset=utf-8")
+	w.Write([]byte (ret))
+}
