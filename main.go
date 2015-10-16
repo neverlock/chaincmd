652c871e0f54eec7d1518cdc66195b11731a5f8e
neverlock <neverlock@gmail.com>
2015-10-16 08:58:45 +0000

    use httplog lib

--- main.go
+++ main.go
@@ -1,59 +1,53 @@
 package main
-import (
-	"fmt"
-	"log"
-	"html"
-	"strings"
-	"os/exec"
-	"net/http"
-	"github.com/gorilla/mux"
-	"code.google.com/p/gcfg"
-	"bytes"
-	)
 
+import (
+	"bytes"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/neverlock/utility/httplog"
+	"gopkg.in/gcfg.v1"
+	"log"
+	"net/http"
+	"os/exec"
+	"strings"
+)
 
 type Config struct {
 	Copycat struct {
-	Cmd string
-	Arg string
-	Bind string
+		Cmd  string
+		Arg  string
+		Bind string
 	}
 }
 
 var cfg Config
 
-func httplog(r *http.Request){
-        log.Printf("%s - %s - %s - %s - %q",
-                r.RemoteAddr,
-                r.Proto,
-                r.Method,
-                r.UserAgent(),
-                html.EscapeString(r.URL.Path),
-        )
-}
-
-
-
-func main(){
+func main() {
 
 	err := gcfg.ReadFileInto(&cfg, "chaincmd.gcfg")
 	if err != nil {
-	    log.Fatalf("Failed to parse gcfg data: %s", err)
+		log.Fatalf("Failed to parse gcfg data: %s", err)
 	}
 
 	rtr := mux.NewRouter()
-	rtr.HandleFunc("/run",runcmd).Methods("GET").Queries("ID","{ID}")
-        http.Handle("/", rtr)
+	rtr.HandleFunc("/run", runcmd).Methods("GET").Queries("ID", "{ID}")
+	http.Handle("/", rtr)
+	rtr.NotFoundHandler = http.HandlerFunc(notFound)
 	bind := cfg.Copycat.Bind
-        fmt.Printf("listening on %s...\n", bind)
-        http.ListenAndServe(bind, nil)
+	fmt.Printf("listening on %s...\n", bind)
+	http.ListenAndServe(bind, nil)
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Server", "Neverlock Chaincmd")
+	w.Write([]byte("404 End point not found"))
 }
 
 func runcmd(w http.ResponseWriter, r *http.Request) {
-	httplog(r)
+	httplog.HttpLogln(r)
 	params := mux.Vars(r)
-	Arg := strings.Replace(cfg.Copycat.Arg,"ID",params["ID"],-1)
-	cmd := exec.Command(cfg.Copycat.Cmd,Arg)
+	Arg := strings.Replace(cfg.Copycat.Arg, "ID", params["ID"], -1)
+	cmd := exec.Command("/bin/sh", "-c", cfg.Copycat.Cmd, Arg)
 
 	var out bytes.Buffer
 	var stderr bytes.Buffer
@@ -67,7 +61,7 @@ func runcmd(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("STDOUT:", out.String())
 
-	ret := fmt.Sprintf("{\"Cmd\":\"%s\",\"Arg\":\"%s\"}",cfg.Copycat.Cmd,Arg)
+	ret := fmt.Sprintf("{\"Cmd\":\"%s\",\"Arg\":\"%s\"}", cfg.Copycat.Cmd, Arg)
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
-	w.Write([]byte (ret))
+	w.Write([]byte(ret))
 }
--- main_goroutine.go
+++ main_goroutine.go
@@ -1,58 +1,56 @@
 package main
-import (
-	"fmt"
-	"log"
-	"html"
-	"strings"
-	"os/exec"
-	"net/http"
-	"github.com/gorilla/mux"
-	"code.google.com/p/gcfg"
-	"bytes"
-	)
 
+import (
+	"bytes"
+	"code.google.com/p/gcfg"
+	"fmt"
+	"github.com/gorilla/mux"
+	"html"
+	"log"
+	"net/http"
+	"os/exec"
+	"strings"
+)
 
 type Config struct {
 	Copycat struct {
-	Cmd string
-	Arg string
-	Arg1 string
-	Bind string
+		Cmd  string
+		Arg  string
+		Arg1 string
+		Bind string
 	}
 }
 
 var cfg Config
 
-func httplog(r *http.Request){
-        log.Printf("%s - %s - %s - %s - %q",
-                r.RemoteAddr,
-                r.Proto,
-                r.Method,
-                r.UserAgent(),
-                html.EscapeString(r.URL.Path),
-        )
+func httplog(r *http.Request) {
+	log.Printf("%s - %s - %s - %s - %q",
+		r.RemoteAddr,
+		r.Proto,
+		r.Method,
+		r.UserAgent(),
+		html.EscapeString(r.URL.Path),
+	)
 }
 
-
-
-func main(){
+func main() {
 
 	err := gcfg.ReadFileInto(&cfg, "chaincmd.gcfg")
 	if err != nil {
-	    log.Fatalf("Failed to parse gcfg data: %s", err)
+		log.Fatalf("Failed to parse gcfg data: %s", err)
 	}
 	//var outPut chan string = make(chan string)
 
 	rtr := mux.NewRouter()
-	rtr.HandleFunc("/run",runcmd).Methods("GET").Queries("ID","{ID}")
-        http.Handle("/", rtr)
+	rtr.HandleFunc("/run", runcmd).Methods("GET").Queries("ID", "{ID}")
+	http.Handle("/", rtr)
 	bind := cfg.Copycat.Bind
-        fmt.Printf("listening on %s...\n", bind)
-        http.ListenAndServe(bind, nil)
+	fmt.Printf("listening on %s...\n", bind)
+	http.ListenAndServe(bind, nil)
 }
 
-func execmd(cmd string,arg1 string,arg2 string) {
-	exe := exec.Command(cmd,arg1,arg2)
+func execmd(cmd string, arg1 string, arg2 string) {
+	exe := exec.Command(cmd, arg1, arg2)
 	var out bytes.Buffer
 	var stderr bytes.Buffer
 	exe.Stdout = &out
@@ -68,12 +66,12 @@ func execmd(cmd string,arg1 string,arg2 string) {
 func runcmd(w http.ResponseWriter, r *http.Request) {
 	httplog(r)
 	params := mux.Vars(r)
-	Arg := strings.Replace(cfg.Copycat.Arg,"ID",params["ID"],-1)
-	log.Println("Arg := ",Arg)
+	Arg := strings.Replace(cfg.Copycat.Arg, "ID", params["ID"], -1)
+	log.Println("Arg := ", Arg)
 
-	go execmd(cfg.Copycat.Cmd,Arg,cfg.Copycat.Arg1)
+	go execmd(cfg.Copycat.Cmd, Arg, cfg.Copycat.Arg1)
 
-	ret := fmt.Sprintf("{\"Cmd\":\"%s\",\"Arg\":\"%s\"}",cfg.Copycat.Cmd,Arg)
+	ret := fmt.Sprintf("{\"Cmd\":\"%s\",\"Arg\":\"%s\"}", cfg.Copycat.Cmd, Arg)
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
-	w.Write([]byte (ret))
+	w.Write([]byte(ret))
 }
